1e00384aed8240efea20f15e4de55b9fb1808dd0
Dmitry Gusev <dmtrxiii@gmail.com>
2022-08-25 18:34:56 +0300

    реализован класс DrugDomain

---
+++ PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/domain/entity/DrugDomain.kt
@@ -0,0 +1,10 @@
+package pro.fateeva.pillsreminder.domain.entity
+
+/**
+ * Поля класса, возможно, будуд скорректированы в зависимости от роста функционала приложения
+ */
+data class DrugDomain(
+    val ID: String,
+    val drugName: String,
+    val description: String,
+)
