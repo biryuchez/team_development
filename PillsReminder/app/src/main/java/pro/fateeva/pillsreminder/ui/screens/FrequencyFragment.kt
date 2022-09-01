6bf42cc8af715169979882211b19af3da404cc61
Dmitry Gusev <dmtrxiii@gmail.com>
2022-09-01 15:01:02 +0300

    скорректирована разметка экрана FrequencyFragment

--- PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/ui/screens/FrequencyFragment.kt
+++ PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/ui/screens/FrequencyFragment.kt
@@ -1,6 +1,9 @@
 package pro.fateeva.pillsreminder.ui.screens
 
+import android.os.Bundle
+import android.view.View
 import androidx.core.os.bundleOf
+import pro.fateeva.pillsreminder.R
 import pro.fateeva.pillsreminder.databinding.FragmentFrequencyBinding
 
 private const val DRUG_NAME_ARG_KEY = "DRUG_NAME"
@@ -12,6 +15,27 @@ class FrequencyFragment :
         fun newInstance(drugName: String): FrequencyFragment {
             return FrequencyFragment().apply {
                 arguments = bundleOf(DRUG_NAME_ARG_KEY to drugName)
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.twicePerDayRadioButton.isEnabled = false // временно отключены все варианты, кроме "Один раз в день"
+        binding.whenNeededRadioButton.isEnabled = false // временно отключены все варианты, кроме "Один раз в день"
+
+        val selectedDrugName = arguments?.getString(DRUG_NAME_ARG_KEY)
+            ?: getString(R.string.arguments_error)
+
+        binding.frequencyQuestionTextView.text =
+            getString(R.string.medication_frequency_header, selectedDrugName)
+
+        binding.frequencyNextScreenButton.setOnClickListener {
+            when (binding.frequencyRadioGroup.checkedRadioButtonId) {
+                binding.oncePerDayRadioButton.id -> {
+                    navigator.navigateToOncePerDayScreen(selectedDrugName)
+                }
             }
         }
     }
--- PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/ui/screens/OncePerDaySettingsFragment.kt
+++ PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/ui/screens/OncePerDaySettingsFragment.kt
@@ -2,10 +2,12 @@ package pro.fateeva.pillsreminder.ui.screens
 
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import pro.fateeva.pillsreminder.databinding.FragmentOncePerDaySettingsBinding
 
 private const val DRUG_NAME_ARG_KEY = "DRUG_NAME"
 
-class OncePerDaySettingsFragment : Fragment() {
+class OncePerDaySettingsFragment :
+    BaseFragment<FragmentOncePerDaySettingsBinding>(FragmentOncePerDaySettingsBinding::inflate) {
 
     companion object {
         fun newInstance(drugName: String): OncePerDaySettingsFragment {
