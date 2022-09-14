fe8bc8dacdc902cd7d83e5a81f7b8a1f65ce1984
Dmitry Gusev <dmtrxiii@gmail.com>
2022-09-14 17:45:38 +0300

    реализован фейковый список дат для календарной сетки

--- PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/ui/screens/HistoryCalendarFragment.kt
+++ PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/ui/screens/HistoryCalendarFragment.kt
@@ -1,5 +1,6 @@
 package pro.fateeva.pillsreminder.ui.screens
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,63 +9,111 @@ import androidx.core.view.children
 import com.google.android.material.card.MaterialCardView
 import pro.fateeva.pillsreminder.R
 import pro.fateeva.pillsreminder.databinding.FragmentHistoryCalendarBinding
-import pro.fateeva.pillsreminder.databinding.HistoryCalendarItemBinding
+import pro.fateeva.pillsreminder.databinding.ItemHistoryCalendarBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val DEFAULT_LAYOUT_SIZE = 0
+private const val LAYOUT_DIMENSION_RATIO = "W,1:1"
+private const val DATE_FORMAT_PATTERN = "dd.MM.yyyy"
+private const val DATE_DELIMITER = '.'
 
 class HistoryCalendarFragment :
     BaseFragment<FragmentHistoryCalendarBinding>(FragmentHistoryCalendarBinding::inflate) {
 
+    @SuppressLint("SimpleDateFormat")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val rowsCount = 5
         val columnsCount = 3
 
-        repeat(rowsCount * columnsCount) {
-            val newViewBinding =
-                HistoryCalendarItemBinding.inflate(LayoutInflater.from(requireContext()))
-            val newView = newViewBinding.root
-            newView.id = it + 1
+        val dateList = mutableListOf<Date>()
 
-            newViewBinding.historyCalendarItemCard.setOnClickListener {
-                binding.calendarContainer.children.iterator().forEachRemaining { parentView ->
-                    parentView.findViewById<MaterialCardView>(R.id.history_calendar_item_card)
-                        .setCardBackgroundColor(requireContext().getColor(R.color.app_primary))
-                }
-                newViewBinding.historyCalendarItemCard.setCardBackgroundColor(requireContext().getColor(
-                    R.color.teal_200))
+        val dateFormat = SimpleDateFormat(DATE_FORMAT_PATTERN)
+        val currentDate = dateFormat.format(Calendar.getInstance().time)
+
+        for (i in -7..7) {
+            val calendar = Calendar.getInstance()
+            calendar.add(Calendar.DAY_OF_YEAR, i)
+            val date = dateFormat.format(calendar.time)
+            dateFormat.parse(date)?.let { dateList.add(it) }
+        }
+
+        repeat(rowsCount * columnsCount) { index ->
+            val historyCalendarItemBinding =
+                ItemHistoryCalendarBinding.inflate(LayoutInflater.from(requireContext()))
+
+            val historyCalendarItemView = historyCalendarItemBinding.root.apply {
+                id = index + 1
             }
 
-            val params = ConstraintLayout.LayoutParams(0, 0)
-            params.dimensionRatio = "W,1:1"
-            newView.layoutParams = params
+            with(binding) {
+                with(historyCalendarItemBinding) {
+                    historyCalendarItemCard.setOnClickListener {
+                        calendarContainer.children.iterator().forEachRemaining { parentView ->
+                            parentView
+                                .findViewById<MaterialCardView>(R.id.history_calendar_item_card)
+                                .setCardBackgroundColor(requireContext()
+                                    .getColor(R.color.calendar_item))
+                        }
+                        historyCalendarItemCard.setCardBackgroundColor(
+                            requireContext().getColor(R.color.selected_date)
+                        )
+                    }
 
-            if (it == 0) {
-                params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
-                params.endToStart = newView.id + 1
-                params.topToTop = ConstraintLayout.LayoutParams.PARENT_ID
-            } else {
-                when (it % rowsCount) {
-                    0 -> {
+                    val params =
+                        ConstraintLayout.LayoutParams(DEFAULT_LAYOUT_SIZE, DEFAULT_LAYOUT_SIZE)
+                    params.dimensionRatio = LAYOUT_DIMENSION_RATIO
+                    historyCalendarItemView.layoutParams = params
+
+                    historyCalendarItemCardMmYyTextView.text =
+                        dateFormat.format(dateList[index]).substringAfter(DATE_DELIMITER)
+                    historyCalendarItemCardDayTextView.text =
+                        dateFormat.format(dateList[index]).substringBefore(DATE_DELIMITER)
+
+
+                    if (dateList[index].before(dateFormat.parse(currentDate))) {
+                        historyCalendarItemCardEventMarkerView.setBackgroundColor(
+                            requireContext().getColor(android.R.color.holo_green_light)
+                        )
+                    }
+
+                    if (dateList[index] == dateFormat.parse(currentDate)) {
+                        historyCalendarItemCard.setCardBackgroundColor(
+                            requireContext().getColor(R.color.selected_date)
+                        )
+                    }
+
+                    if (index == 0) {
                         params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
-                        params.endToStart = newView.id + 1
-                        params.topToBottom = newView.id - rowsCount
+                        params.endToStart = historyCalendarItemView.id + 1
+                        params.topToTop = ConstraintLayout.LayoutParams.PARENT_ID
+                    } else {
+                        when (index % rowsCount) {
+                            0 -> {
+                                params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
+                                params.endToStart = historyCalendarItemView.id + 1
+                                params.topToBottom = historyCalendarItemView.id - rowsCount
+                            }
+
+                            in (1..rowsCount - 2) -> {
+                                params.startToEnd = historyCalendarItemView.id - 1
+                                params.endToStart = historyCalendarItemView.id + 1
+                                params.topToTop = historyCalendarItemView.id - 1
+                            }
+
+                            (rowsCount - 1) -> {
+                                params.startToEnd = historyCalendarItemView.id - 1
+                                params.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
+                                params.topToTop = historyCalendarItemView.id - 1
+                            }
+                        }
                     }
 
-                    in (1..rowsCount - 2) -> {
-                        params.startToEnd = newView.id - 1
-                        params.endToStart = newView.id + 1
-                        params.topToTop = newView.id - 1
-                    }
-
-                    (rowsCount - 1) -> {
-                        params.startToEnd = newView.id - 1
-                        params.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
-                        params.topToTop = newView.id - 1
-                    }
+                    calendarContainer.addView(historyCalendarItemView)
                 }
             }
-
-            binding.calendarContainer.addView(newView)
         }
     }
 }
