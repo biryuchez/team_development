969b346f16d649f35aeeff35437fbc8333fb70f0
Dmitry Gusev <dmtrxiii@gmail.com>
2022-09-25 10:07:39 +0300

    фикс логики метода чтения напоминаний из БД

--- PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/clean/data/MedicationReminderRepositoryImpl.kt
+++ PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/clean/data/MedicationReminderRepositoryImpl.kt
@@ -1,13 +1,10 @@
 package pro.fateeva.pillsreminder.clean.data
 
-import pro.fateeva.pillsreminder.clean.data.room.MedicationReminderDao
 import pro.fateeva.pillsreminder.clean.data.room.MedicationEntityMapper
 import pro.fateeva.pillsreminder.clean.data.room.MedicationIntakeDao
+import pro.fateeva.pillsreminder.clean.data.room.MedicationReminderDao
 import pro.fateeva.pillsreminder.clean.domain.entity.MedicationReminder
 import pro.fateeva.pillsreminder.clean.domain.entity.MedicationScheduleItemDomain
-import pro.fateeva.pillsreminder.extensions.getDayBeginningTime
-import pro.fateeva.pillsreminder.extensions.getDayEndTime
-import java.util.*
 
 private const val ONE_DAY_IN_MILLIS = 86_400_000L
 
@@ -18,10 +15,7 @@ class MedicationReminderRepositoryImpl(
 ) : MedicationReminderRepository {
 
     override fun saveMedicationReminder(medicationReminder: MedicationReminder) {
-        val calendar = Calendar.getInstance()
-        val currentTime = calendar.timeInMillis
-
-        intakeDao.deletePlannedIntakes(medicationReminder.id, currentTime)
+        intakeDao.deletePlannedIntakes(medicationReminder.id, System.currentTimeMillis())
 
         reminderDao.addMedicationReminder(
             mapper.mapMedicationReminderDomainToEntity(medicationReminder)
@@ -45,31 +39,25 @@ class MedicationReminderRepositoryImpl(
     }
 
     override fun getMedicationReminder(id: Int): MedicationReminder {
-        val calendar = Calendar.getInstance()
-
         return mapper.mapMedicationReminderEntityToDomain(
             reminderDao.getMedicationReminder(id),
-            intakeDao.getMedicationIntakes(
+            intakeDao.getMedicationIntakesForNext24H(
                 id,
-                calendar.getDayBeginningTime(),
-                calendar.getDayEndTime()
+                System.currentTimeMillis()
             )
         )
     }
 
     override fun getMedicationReminders(): List<MedicationReminder> {
-        val calendar = Calendar.getInstance()
-
         val medicationRemindersList = mutableListOf<MedicationReminder>()
 
         for (medicationReminderEntity in reminderDao.getAllMedicationReminders()) {
             medicationRemindersList.add(
                 mapper.mapMedicationReminderEntityToDomain(
                     medicationReminderEntity,
-                    intakeDao.getMedicationIntakes(
+                    intakeDao.getMedicationIntakesForNext24H(
                         medicationReminderEntity.pillID,
-                        calendar.getDayBeginningTime(),
-                        calendar.getDayEndTime()
+                        System.currentTimeMillis()
                     )
                 )
             )
--- PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/clean/data/room/MedicationIntakeDao.kt
+++ PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/clean/data/room/MedicationIntakeDao.kt
@@ -17,8 +17,8 @@ interface MedicationIntakeDao {
     @Query("SELECT * FROM MedicationIntakeEntity WHERE intakeID = :intakeID ORDER BY medicationTime ASC")
     fun getMedicationIntakes(intakeID: Int): List<MedicationIntakeEntity>
 
-    @Query("SELECT * FROM MedicationIntakeEntity WHERE intakeID = :intakeID AND medicationTime >= :startTime AND medicationTime <= :endTime ORDER BY medicationTime ASC")
-    fun getMedicationIntakes(intakeID: Int, startTime: Long, endTime: Long): List<MedicationIntakeEntity>
+    @Query("SELECT * FROM MedicationIntakeEntity WHERE intakeID = :intakeID AND medicationTime >= :currentTime AND medicationTime <= (:currentTime + 86400000) ORDER BY intakeIndex ASC")
+    fun getMedicationIntakesForNext24H(intakeID: Int, currentTime: Long): List<MedicationIntakeEntity>
 
     @Query("DELETE FROM MedicationIntakeEntity WHERE intakeID = :pillID")
     fun deleteMedicationIntake(pillID: Int)
