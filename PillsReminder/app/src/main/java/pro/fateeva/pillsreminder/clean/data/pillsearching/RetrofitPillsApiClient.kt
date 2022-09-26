e7eb1226c7432f34d8fa77b6179799020c07d752
Dmitry Gusev <dmtrxiii@gmail.com>
2022-09-26 11:51:46 +0300

    добавлена проверка на BuildType в клиенте ретрофита

--- PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/clean/data/pillsearching/RetrofitPillsApiClient.kt
+++ PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/clean/data/pillsearching/RetrofitPillsApiClient.kt
@@ -2,14 +2,21 @@ package pro.fateeva.pillsreminder.clean.data.pillsearching
 
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import pro.fateeva.pillsreminder.BuildConfig
 import java.util.concurrent.TimeUnit
 
 class RetrofitPillsApiClient {
     fun createClient(): OkHttpClient {
-        return OkHttpClient.Builder()
+        val client = OkHttpClient.Builder()
+
+        if (BuildConfig.DEBUG) {
+            client.addInterceptor(HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY))
+        }
+
+        return client
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
-            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .build()
     }
 }
--- PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/clean/data/pillsearching/SearchPillsRepository.kt
+++ PillsReminder/app/src/main/java/pro/fateeva/pillsreminder/clean/data/pillsearching/SearchPillsRepository.kt
@@ -2,11 +2,6 @@ package pro.fateeva.pillsreminder.clean.data.pillsearching
 
 import pro.fateeva.pillsreminder.clean.domain.entity.DrugDomain
 
-/**
- * Юзкейс предполагается для репозитория, который будет по API получать список лекарств
- * по поисковому запросу.
- * Скорректируйте при необходимости
- */
 interface SearchPillsRepository {
     suspend fun searchPills(query: String): List<DrugDomain>
 }
